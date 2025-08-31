bd21a64f87e90f5a1b534c8ac72b5640d0b68a86
THE OX SNOOKER <theoxsnooker@gmail.com>
2025-08-31 05:07:20 +0000

    connect all database to firestore

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,10 +2,6 @@
 import type { Table, MenuItem, Admin, Staff, MembershipPlan, Member } from './types';
 
 // This file is now mainly for exporting types.
-// Most data is fetched from Firestore.
-
-export const admins: Admin[] = [
-    { id: 'admin-1', username: 'admin', password: 'password' },
-];
+// All data is now fetched from Firestore.
 
 export type { Table, MenuItem, Admin, Staff, MembershipPlan, Member, ActiveSession, OrderItem, Transaction } from './types';
